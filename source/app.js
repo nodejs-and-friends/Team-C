0066d3d7d4e3c1955e030c88e58da3f03cb813f7
Angel Angelov <angel1914@abv.bg>
2016-11-30 12:58:41 +0200

    Some design improvements

--- source/app.js
+++ source/app.js
@@ -7,16 +7,5 @@ const app = require("./config/application");
 const data = require("./data")(config);
 
 require("./routers")(app, data);
-// console.log(config);
-
-// data.createTeam("Team A")
-// 	.then(() => {
-// 		console.log("Team A created");
-// 	});
-
-// data.getAllTeams()
-// 	.then(() => {
-// 		console.log("Teams listed");
-// 	});
 
 app.listen(config.port, () => console.log(`Running at: ${config.port}`));
