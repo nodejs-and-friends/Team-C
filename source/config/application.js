5b2fe5772b88237e593a2d82fad711c47b194cca
Peter Velkov <kidroca@gmail.com>
2016-12-02 18:36:45 +0200

    refactor(application): added installed modules as app dependencies

--- source/config/application.js
+++ source/config/application.js
@@ -1,8 +1,12 @@
 /* globals require */
+"use strict";
 
-const express = require("express"),
-	bodyParser = require('body-parser'),
- 	methodOverride = require('method-override')
+const secrets = require("../../secrets");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const methodOverride = require("method-override");
 
 let app = express();
 
@@ -10,9 +14,12 @@ app.set("view engine", "pug");
 
 app.use("/static", express.static("public"));
 
-app.use(methodOverride('_method'));
+app.use(methodOverride("_method"));
 
+app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({ secret: secrets.session }));
+
 
 module.exports = app;
