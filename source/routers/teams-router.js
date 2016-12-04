0ea28b5bc4c636e9245f585720cfee3434fe3d9b
Peter Velkov <kidroca@gmail.com>
2016-12-04 16:23:58 +0200

    refactor(routes/team): authorization
    Updated team routes authorization
    - todo team owner middleware

--- source/routers/teams-router.js
+++ source/routers/teams-router.js
@@ -4,22 +4,26 @@
 const express = require("express");
 const app = require("../config/application");
 const controller = require("../controllers/team-controller");
+const authorizedInterceptor = require("../midlleware/authorization-iterceptor");
+
+// extract to middleware and add logic
+const teamOwnerIntercetor = (res, req, next) => next();
 
 let router = new express.Router();
 
 router.get("/", controller.getAll)
-      .post("/accept/:teamId/:userId", controller.acceptUserForTeam)
-      .post("/decline/:teamId/:userId", controller.declineUserForTeam)
-      .post("/removeUser/:teamId/:userId", controller.removeUserForTeam)
-      .get("/create", (req, res) => {
+      .post("/accept/:teamId/:userId", teamOwnerIntercetor, controller.acceptUserForTeam)
+      .post("/decline/:teamId/:userId", teamOwnerIntercetor, controller.declineUserForTeam)
+      .post("/removeUser/:teamId/:userId", teamOwnerIntercetor, controller.removeUserForTeam)
+      .get("/create", authorizedInterceptor, (req, res) => {
           res.render("teams/team-create");
       })
       .get("/:id", controller.getById)
-      .post("/", controller.create)
-      .get("/update/:id", controller.getTeamForUpdateById)
-      .post("/:id", controller.update)
-      .get("/remove/:id", controller.remove)
-      .post("/accept/", controller.acceptUserForTeam)
-      .post("/join/:id", controller.addAppliedUser);
+      .post("/create", authorizedInterceptor, controller.create)
+      .get("/update/:id", teamOwnerIntercetor, controller.getTeamForUpdateById)
+      .post("/:id", teamOwnerIntercetor, controller.update)
+      .get("/remove/:id", teamOwnerIntercetor, controller.remove)
+      .post("/accept/", teamOwnerIntercetor, controller.acceptUserForTeam)
+      .post("/join/:id", teamOwnerIntercetor, controller.addAppliedUser);
 
 app.use("/teams", router);
