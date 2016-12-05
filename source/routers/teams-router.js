5f1724a93250f49f60c133e6c14989079a3da077
Peter Velkov <kidroca@gmail.com>
2016-12-05 10:20:08 +0200

    refactor(routes/team): separated team owner logic

--- source/routers/teams-router.js
+++ source/routers/teams-router.js
@@ -6,22 +6,24 @@ const app = require("../config/application");
 const controller = require("../controllers/team-controller");
 const authorizedInterceptor = require("../midlleware/authorization-iterceptor");
 const teamInterceptor = require("../midlleware/team-interceptor");
-const teamOwnerIntercetor = require("../midlleware/team-owner-interceptor");
+const teamOwnerInterceptor = require("../midlleware/team-owner-interceptor");
 
-let router = new express.Router();
+const teamOwnerInterceptors = [authorizedInterceptor, teamInterceptor, teamOwnerInterceptor];
 
-router.get("/", controller.getAll)
-      .post("/accept/:id/:userId", authorizedInterceptor, teamInterceptor, teamOwnerIntercetor, controller.acceptUserForTeam)
-      .post("/decline/:id/:userId", authorizedInterceptor, teamInterceptor, teamOwnerIntercetor, controller.declineUserForTeam)
-      .post("/removeUser/:id/:userId", authorizedInterceptor, teamInterceptor, teamOwnerIntercetor, controller.removeUserForTeam)
-      .get("/create", authorizedInterceptor, (req, res) => {
-          res.render("teams/team-create");
-      })
-      .get("/:id", controller.getById)
-      .post("/create", authorizedInterceptor, controller.create)
-      .get("/update/:id", authorizedInterceptor, teamInterceptor, teamOwnerIntercetor, controller.getTeamForUpdateById)
-      .post("/:id", authorizedInterceptor, teamInterceptor, teamOwnerIntercetor, controller.update)
-      .get("/remove/:id", authorizedInterceptor, teamInterceptor, teamOwnerIntercetor, controller.remove)
-      .post("/join/:id", authorizedInterceptor, teamInterceptor, controller.addAppliedUser);
+let teamsRouter = new express.Router();
 
-app.use("/teams", router);
+teamsRouter
+    .get("/", controller.getAll)
+    .get("/create", authorizedInterceptor, (req, res) => res.render("teams/team-create"))
+    .post("/create", authorizedInterceptor, controller.create)
+    .post("/join/:id", authorizedInterceptor, teamInterceptor, controller.addAppliedUser)
+    .get("/:id", controller.getById)
+
+    .post("/accept/:id/:userId", teamOwnerInterceptors, controller.acceptUserForTeam)
+    .post("/decline/:id/:userId", teamOwnerInterceptors, controller.declineUserForTeam)
+    .post("/removeUser/:id/:userId", teamOwnerInterceptors, controller.removeUserForTeam)
+    .get("/update/:id", teamOwnerInterceptors, controller.getTeamForUpdateById)
+    .post("/:id", teamOwnerInterceptors, controller.update)
+    .get("/remove/:id", teamOwnerInterceptors, controller.remove);
+
+app.use("/teams", teamsRouter);
